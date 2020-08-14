7142f6ea191ffe3a8b698964011718861ff1a32d
Rezo Megrelidze <rezomegrelidze@gmail.com>
2020-08-14 19:15:01 +0300

    Add project files.

---
+++ RazDIContainer.ConsoleTests/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace RazDIContainer.ConsoleTests
+{
+    class Program
+    {
+        private static DIContainer container = new DIContainer();
+
+        static void Main(string[] args)
+        {
+            container.Register<AnimalService, AnimalService>();
+            container.Register<IAnimal, Cat>();
+            container.RegisterSingleton<ICompany,Company>();
+
+            var service = container.Resolve<AnimalService>();
+
+            service.GetAnimal().MakeSound();
+
+        }
+    }
+
+    public interface ICompany
+    {
+        string Name { get; set; }
+    }
+
+    public class Company : ICompany
+    {
+        public string Name { get; set; }
+    }
+
+    public interface IAnimal
+    {
+        void MakeSound();
+    }
+
+    public class AnimalService
+    {
+        private readonly IAnimal _animal;
+
+        public AnimalService(IAnimal animal)
+        {
+            _animal = animal;
+        }
+
+        public IAnimal GetAnimal() => _animal;
+    }
+
+    public class Dog : IAnimal
+    {
+        public void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    public class Cat : IAnimal
+    {
+        public void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+}
---
+++ RazDIContainer/DIContainer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RazDIContainer
+{
+    public class DIContainer
+    {
+        private readonly Dictionary<Type, Type> resolver;
+        private readonly Dictionary<Type, object> singletons;  
+
+        public DIContainer()
+        {
+            resolver = new Dictionary<Type, Type>();
+            singletons = new Dictionary<Type, object>();
+        }
+
+        public void Register<TAbstract, TConcrete>()
+        {
+            resolver[typeof(TAbstract)] = typeof(TConcrete);
+        }
+
+        public void RegisterSingleton<TAbstract, TConcrete>()
+        {
+            resolver[typeof(TAbstract)] = typeof(TConcrete);
+            singletons[typeof(TAbstract)] = Resolve<TAbstract>();
+        }
+
+        public TAbstract Resolve<TAbstract>()
+        {
+            return (TAbstract) Resolve(typeof(TAbstract));
+        }
+
+        private object Resolve(Type type)
+        {
+            if (singletons.ContainsKey(type)) return singletons[type];
+            if (HasSingleParameterlessConstructor(type) || !type.GetConstructors().Any())
+            {
+                return Activator.CreateInstance(resolver[type]) ;
+            }
+
+            var constructor = type
+                .GetConstructors().First();
+
+            var parameters = constructor.GetParameters().Select(p =>
+            {
+                if (!resolver.ContainsKey(p.ParameterType)) return Activator.CreateInstance(p.ParameterType);
+                var getInstance = Resolve(p.ParameterType);
+                return getInstance;
+            });
+
+            return Activator.CreateInstance(type, parameters.ToArray());
+        }
+
+        private bool HasSingleParameterlessConstructor(Type type)
+        {
+            var constructors = type.GetConstructors();
+            return constructors.Length == 1 && !constructors.Single().GetParameters().Any();
+        }
+    }
+}
